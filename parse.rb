a1d90f031e2332106be0928f4c8e021061c96de3
Nat Welch <nat@natwelch.com>
2014-06-11 14:38:25 -0700

    this pulls out the nouns

--- parse.rb
+++ parse.rb
@@ -1,8 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 
-# Get a Nokogiri::HTML::Document for the page we’re interested in...
-#
-doc = Nokogiri::XML(open('./gcide_xml/gcide_a.xml'))
-p doc
-p doc.css('p')
+filename = File.expand_path '~/Downloads/gcide-0.51/CIDE.A'
+@db = {}
+File.open(filename, "r:ISO-8859-1") do |file|
+  chunks = file.read.split(/\n\n/).select{|chunk| chunk =~ /^[<\[]\w/}    
+
+    chunks.each do |chunk|
+      doc = Nokogiri::XML(chunk)
+      ent = doc.css("ent").map{|n| n.text}.join(" ")
+      pos = doc.css("pos").map{|n| n.text}.join(" ")
+      if pos != "" and ent != ""
+        @db[ent] = pos.split(/\s+/).map{|p| p.gsub(/\./, "")}.join(" ")
+      end
+    end
+end
+
+p @db
