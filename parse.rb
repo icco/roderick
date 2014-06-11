c04765ca22a936a2e1b957fa147149c4715f5134
Nat Welch <nat@natwelch.com>
2014-06-11 14:53:35 -0700

    moving gcide around

--- parse.rb
+++ parse.rb
@@ -1,19 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
 
-filename = File.expand_path '~/Downloads/gcide-0.51/CIDE.A'
+filename = File.expand_path './gcide_xml/gcide_a.xml'
 @db = {}
 File.open(filename, "r:ISO-8859-1") do |file|
-  chunks = file.read.split(/\n\n/).select{|chunk| chunk =~ /^[<\[]\w/}    
+  chunks = file.read.split(/\n\n/).select{|chunk| chunk =~ /^[<\[]\w/ }
 
-    chunks.each do |chunk|
-      doc = Nokogiri::XML(chunk)
-      ent = doc.css("ent").map{|n| n.text}.join(" ")
-      pos = doc.css("pos").map{|n| n.text}.join(" ")
-      if pos != "" and ent != ""
-        @db[ent] = pos.split(/\s+/).map{|p| p.gsub(/\./, "")}.join(" ")
-      end
+  chunks.each do |chunk|
+    doc = Nokogiri::XML(chunk)
+    ent = doc.css("ent").map{|n| n.text }.join(" ")
+    pos = doc.css("pos").map{|n| n.text }.join(" ")
+    dei = doc.css("def").map{|n| n.text }.join(" ")
+    if pos != "" and ent != "" and !dei.empty?
+      #@db[ent] = pos.split(/\s+/).map{|p| p.gsub(/\./, "")}.join(" ")
+      @db[ent] = dei
     end
+  end
 end
 
 p @db
