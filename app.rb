fe9dd7df28828901d32648942f0e8f6837dd6885
Nat Welch <nat@natwelch.com>
2014-06-16 17:35:15 -0700

    Basic pagination

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+PER_PAGE = 100
+
 get '/' do
   dict = Dictionary.load
   @data = dict.data
@@ -7,5 +9,19 @@ end
 get '/words.json' do
   dict = Dictionary.load
   content_type :json
-  Oj.dump(dict.data.to_a.map {|e| Hash['word', e[0], 'definition', e[1]] })
+  Oj.dump(dict.to_a)
+end
+
+get '/words/:page.json' do
+  page = params["page"].to_i
+  if page < 1
+    redirect to('/words/1.json')
+  end
+
+  dict = Dictionary.load
+  start = (page - 1) * PER_PAGE
+  finish = page * PER_PAGE
+
+  content_type :json
+  Oj.dump(dict.to_a[start..finish])
 end
--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -5,6 +5,10 @@ class Dictionary
     @data = {}
   end
 
+  def to_a
+    self.data.to_a.map {|e| Hash['word', e[0], 'definition', e[1]] }
+  end
+
   def self.load
     dict = Dictionary.new
     Zlib::GzipReader.open('dict.json.gz') {|gz| dict.data = Oj.load(gz.read) }
